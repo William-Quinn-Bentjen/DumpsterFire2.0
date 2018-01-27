e6432efa2a57bba85af53c7926cfbfcb513e208a
Unknown <Druffel12@gmail.com>
2018-01-27 02:15:39 -0800

    Character type change work

---
+++ Assets/Interaction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    struct Civillian
+    {
+       
+    }
+
+    bool alive = true;
+
+    struct Zombie
+    {
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+    void onTriggerEnter(Collider Civillian)
+    {
+        alive = false;
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (alive == false)
+        {
+            //AI switched to zombie script
+            Civillian = Zombie;
+        }
+        else
+        {
+//AI stays with current alive script
+            
+        }
+	}
+}
